267b24d03831a5a329ffee3697cd36cbcacb5f8d
maxi758 <maxi758@gmail.com>
2020-10-01 10:58:12 -0300

    añadidas clases

---
+++ Cadete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpnro3_maxi758
+{
+    class Cadete
+    {
+        static int identificador = 0;
+        int id;
+        String nombre;
+        String direccion;
+        String telefono;
+        List<Pedido> ListadoDePedidos;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        internal List<Pedido> ListadoPedidos { get => ListadoPedidos; set => ListadoPedidos = value; }
+    }
+}
---
+++ Cadeteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpnro3_maxi758
+{
+    class Cadeteria
+    {
+        String nombre;
+        List<Cadete> ListadoDeCadetes;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        internal List<Cadete> ListadoDeCadetes1 { get => ListadoDeCadetes; set => ListadoDeCadetes = value; }
+    }
+}
